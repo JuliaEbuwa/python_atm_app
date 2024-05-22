6d399557a41d4bf193bec0cca08f4dc811a53c54
JuliaEbuwa <ebuwa.iguobaro@decagon.dev>
2024-05-22 18:28:36 +0100

    Update on utility bills to return current balance, update on utility bills to break out of its loop to the main file when user inputs option to cancel

--- Bank_App/Recharge.py
+++ Bank_App/Recharge.py
@@ -2,3 +2,4 @@ def recharge(phone_number, amount, current_balance):
     print(f"{phone_number} has been recharged with {amount}")
     current_balance -= amount
     print(f"current account balance is {current_balance}")
+    return current_balance
--- Bank_App/dstv_subscription.py
+++ Bank_App/dstv_subscription.py
@@ -2,4 +2,5 @@ def subscription(dstv_number, amount, current_balance):
     print(f"{dstv_number} has been recharged with {amount}")
     current_balance -= amount
     print(f"current account balance is {current_balance}")
+    return current_balance
 
