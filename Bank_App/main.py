1d8c1e31abe1c84abff56497c462daca5c488918
JuliaEbuwa <ebuwa.iguobaro@decagon.dev>
2024-05-26 17:59:01 +0100

    handled exceptions for user input in the main module

--- Bank_App/main.py
+++ Bank_App/main.py
@@ -7,24 +7,36 @@ retry = 0
 customersAndPin = {1234: "John Solomon", 2345: "Cynthia David", 3456: "Grace Peter"}
 
 while retry <= 2:
-    pin = int(input("Enter your pin: "))
+    try:
+        pin = int(input("Enter your pin: "))
+    except ValueError:
+        retry +=1
+        if retry == 1:
+            print("two more trials, kindly enter a valid input")
+        elif retry == 2:
+            print("one more trial, kindly enter a valid input")
+        else: 
+            print("You have been logged out, kindly log in again!")
+        continue
     if pin in customersAndPin.keys():
         print("Login successful!")
         print(f"Welcome {customersAndPin.get(pin)}!")
         while True:
-            user_input = int(input("What would you like to do today? \n"""
-                                   "Press 1 to Check account balance \n"
-                                   "Press 2 to Withdraw \n"
-                                   "Press 3 to Deposit \n"
-                                   "Press 4 to Pay Utility bill \n"
-                                   "Press 5 to Exit \n: "))
+            try:
+                user_input = int(input("What would you like to do today? \n"""
+                                    "Press 1 to Check account balance \n"
+                                    "Press 2 to Withdraw \n"
+                                    "Press 3 to Deposit \n"
+                                    "Press 4 to Pay Utility bill \n"
+                                    "Press 5 to Exit \n: "))
+            except ValueError:
+                print("Error! value must be an integer")
+                continue
             if user_input == 1:
                 print(f"Account balance is: {current_balance}")
-
             elif user_input == 2:
                 amount = float(input("Enter amount to withdraw: "))
                 current_balance = withdrawal.withdraw(current_balance, amount)
-
             elif user_input == 3:
                 amount = float(input("Enter amount to deposit: "))
                 current_balance = Deposit.deposit(current_balance, amount)
